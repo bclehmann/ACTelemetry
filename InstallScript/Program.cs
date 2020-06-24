48bf009b5d46ccc3a22dd794f255e9a482561cdc
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-06-23 21:17:50 -0600

    #8

---
+++ InstallScript/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace InstallScript
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string invoker_dir = Directory.GetCurrentDirectory();
+
+			string target_dir = "C:/Program Files/ACTelemetry";
+
+			if (!Directory.Exists(target_dir))
+			{
+				Directory.CreateDirectory(target_dir);
+			}
+
+			if(File.Exists(target_dir + "/Backend.dll"))
+			{
+				File.Delete(target_dir + "/Backend.dll");
+			}
+
+			File.Move(invoker_dir + "/Backend.dll", target_dir + "/Backend.dll");
+		}
+	}
+}
